868878d83ca00ac30005d1129b2bda7eb0f79ceb
Money Trees <matveyshakur@gmail.com>
2023-01-24 14:50:49 +0500

    refactor: зарефакторил код в ООП стиле

--- script.js
+++ script.js
@@ -11,32 +11,96 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// использую geolocation api
-if (navigator.geolocation) {
-  // принимает два колбэка - на success и на error
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // деструктуризация объекта
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+// строим архитектуру в ООП стиле
+class App {
+  // приватные поля
+  #map;
+  #mapEvent;
 
-      const coords = [latitude, longitude];
+  constructor() {
+    // прогружаем карту
+    this._getPosition();
 
-      const map = L.map("map").setView(coords, 13);
+    // отображаем форму ввода
+    // снова биндим, потому что this в eventHandler показывает на HTML элемент,
+    // то есть на форму в данном случае, чтобы указывал на объект, нужно биндить
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
-    },
-    function () {
-      alert("Could not get your position");
+  // метод, позволяющий получить координаты и отрендерить карту
+  _getPosition() {
+    // использую geolocation api
+    if (navigator.geolocation) {
+      // принимает два колбэка - на success и на error
+      navigator.geolocation.getCurrentPosition(
+        // когда мы подаем ф-ию без вызова, но нам нужен this, то биндим его сами
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
+      );
     }
-  );
+  }
+
+  // рендер карты
+  _loadMap(position) {
+    // деструктуризация объекта для получения координат
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    // создаем массив координат для leaflet lib
+    const coords = [latitude, longitude];
+    // рендер карты
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // event listener в leaflet lib
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // чистим инпуты
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    // Отображение маркера
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+  }
 }
+
+const app = new App();
